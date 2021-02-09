b2c9fc541867b0a52354c7fe60458efcb22e4eb2
EC2 Default User <ec2-user@ip-172-31-7-180.ec2.internal>
2021-02-09 04:34:27 +0000

    lecture 4

---
+++ lecture4.py
@@ -0,0 +1,17 @@
+"""
+lecture 4
+"""
+
+my_tuple = 'a','b','c','d','e'
+print(my_tuple)
+
+my_2nd_tuple = ('a','b','c','d','e')
+print(my_2nd_tuple)
+
+not_a_tuple = ('a')
+print(type(not_a_tuple))
+
+is_a_tuple = ('a',)
+print(type(is_a_tuple))
+
+print(my_tuple[:])
